c27747d37648da4615e0b2c7793e94ad794eb5ee
WilisArum02 <148854173+WilisArum02@users.noreply.github.com>
2023-10-24 15:45:09 +0700

    Add files via upload

---
+++ LatihanGuiPBO56/src/latihanguipbo5dan6/Dosen.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihanguipbo5dan6;
+
+/**
+ *
+ * @author HP
+ */
+public class Dosen {
+
+    private String nidn;
+    private String nama;
+    private String alamat;
+    private String gender;
+
+    /**
+     * @return the nidn
+     */
+    public String getNidn() {
+        return nidn;
+    }
+
+    /**
+     * @param nidn the nidn to set
+     */
+    public void setNidn(String nidn) {
+        this.nidn = nidn;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+}
